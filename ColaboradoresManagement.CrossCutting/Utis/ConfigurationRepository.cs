1e235326f4750f43de4a09ea4d9ec4e75cc8255c
Herbert Carvalho <hscarvalho@outlook.com.br>
2021-12-23 20:17:09 -0300

    Correção ConnectionString

--- ColaboradoresManagement.CrossCutting/Utis/ConfigurationRepository.cs
+++ ColaboradoresManagement.CrossCutting/Utis/ConfigurationRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ColaboradoresManagement.CrossCutting.Utis
+﻿namespace ColaboradoresManagement.CrossCutting.Utis
 {
     public class ConfigurationRepository
     {       
