2b919d589959b7cc5ee1a1c30a459eb75c9fab43
Herbert Carvalho <hscarvalho@outlook.com.br>
2021-12-23 17:02:26 -0300

    Adicionando classes e preparando para adicionar banco de dados a aplicação.

--- ColaboradoresManagement/Controllers/ColaboradoresController.cs
+++ ColaboradoresManagement/Controllers/ColaboradoresController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ColaboradoresManagement.Domain.Interface.Service;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,22 @@ namespace ColaboradoresManagement.Controllers
 {
     public class ColaboradoresController : Controller
     {
+        private readonly IGerenciarColaboradoresService _gerenciarColaboradoresService;
+
+        public ColaboradoresController(IGerenciarColaboradoresService gerenciarColaboradoresService)
+        {
+            _gerenciarColaboradoresService = gerenciarColaboradoresService;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
+        public async Task<IActionResult> BuscarColaboradoresAsync()
+        {
+            return null;
+        }
+
     }
 }
